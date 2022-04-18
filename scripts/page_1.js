a4593f8207cdb364c5ae1bf67a98551915ad7233
user8520 <103239607+user8520@users.noreply.github.com>
2022-04-18 07:55:27 +0300

    Add files via upload

---
+++ scripts/page_1.js
@@ -0,0 +1,72 @@
+function pr() {
+    var pr = document.getElementById('pr');
+    pr.style.visibility='visible';
+    var arifm = document.getElementById('arifm');
+    arifm.style.visibility='hidden';
+    var type = document.getElementById('type');
+    type.style.visibility='hidden';
+    var cikl = document.getElementById('cikl');
+    cikl.style.visibility='hidden';
+    var cikl_ef = document.getElementById('cikl_ef');
+    cikl_ef.style.visibility='hidden';
+    var div_img = document.getElementById('div_img');
+    div_img.style.visibility='hidden';
+}
+
+function arifm() {
+    var arifm = document.getElementById('arifm');
+    arifm.style.visibility='visible';
+    var pr = document.getElementById('pr');
+    pr.style.visibility='hidden';
+    var type = document.getElementById('type');
+    type.style.visibility='hidden';
+    var cikl = document.getElementById('cikl');
+    cikl.style.visibility='hidden';
+    var cikl_ef = document.getElementById('cikl_ef');
+    cikl_ef.style.visibility='hidden';
+    var div_img = document.getElementById('div_img');
+    div_img.style.visibility='hidden';
+}
+
+function tipe() {
+    var type = document.getElementById('type')
+    type.style.visibility='visible'
+    var pr = document.getElementById('pr');
+    pr.style.visibility='hidden';
+    var arifm = document.getElementById('arifm');
+    arifm.style.visibility='hidden';
+    var cikl = document.getElementById('cikl');
+    cikl.style.visibility='hidden';
+    var cikl_ef = document.getElementById('cikl_ef');
+    cikl_ef.style.visibility='hidden';
+    var div_img = document.getElementById('div_img');
+    div_img.style.visibility='hidden';
+}
+function cikl() {
+    var cikl = document.getElementById('cikl');
+    cikl.style.visibility='visible';
+    var type = document.getElementById('type')
+    type.style.visibility='hidden'
+    var pr = document.getElementById('pr');
+    pr.style.visibility='hidden';
+    var arifm = document.getElementById('arifm');
+    arifm.style.visibility='hidden';
+    var cikl_ef = document.getElementById('cikl_ef');
+    cikl_ef.style.visibility='hidden';
+    var div_img = document.getElementById('div_img');
+    div_img.style.visibility='hidden';
+}
+function cikl_ef() {
+    var cikl_ef = document.getElementById('cikl_ef');
+    cikl_ef.style.visibility='visible';
+    var cikl = document.getElementById('cikl');
+    cikl.style.visibility='hidden';
+    var type = document.getElementById('type')
+    type.style.visibility='hidden'
+    var pr = document.getElementById('pr');
+    pr.style.visibility='hidden';
+    var arifm = document.getElementById('arifm');
+    arifm.style.visibility='hidden';
+    var div_img = document.getElementById('div_img');
+    div_img.style.visibility='hidden';
+}
---
+++ scripts/page_3.js
@@ -0,0 +1,8 @@
+function sent() {
+    var im = document.getElementById('im');
+    im.setAttribute("values", )
+    var textim = im.getAttribute("value");
+    var sms = document.createElement('p');
+    sms.innerText=textim;
+    document.getElementById("ul").appendChild(sms);
+}
